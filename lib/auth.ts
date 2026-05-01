a18f86216f7b1d56142ae7e89f268d82eeac835e
unknown <fauziaditya874@gmail.com>
2026-05-01 16:59:15 +0700

    fix: automatic baseURL detection for better-auth local and prod

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,7 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
   secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL!,
+  baseURL: process.env.NODE_ENV === "production" ? "https://learnify-lms-one.vercel.app" : "http://localhost:3000",
   trustedOrigins: [
     "https://learnify-lms-one.vercel.app",
     "http://localhost:3000"
