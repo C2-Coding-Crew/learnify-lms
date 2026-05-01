1c6ab540f671ef1fe4a9264d97d999f370fff2c6
unknown <fauziaditya874@gmail.com>
2026-05-01 16:26:13 +0700

    new

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,11 +9,10 @@ export const auth = betterAuth({
     provider: "postgresql",
   }),
   secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL!,
   trustedOrigins: [
     "https://learnify-lms-one.vercel.app",
-    "http://localhost:3000",
-    "learnify-lms-one.vercel.app"
+    "http://localhost:3000"
   ],
   trustHost: true,
 
